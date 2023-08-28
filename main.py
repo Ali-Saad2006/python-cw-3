946970ddadc05dabf49b47b1f4b26f155a1e92d2
ali saad <alizsaadz33@gmail.com>
2023-08-28 19:37:20 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # write your code here
+favorite_animals = [ 'dog', "cat","monkey" , " rabbit"]
+print(favorite_animals )
+print(favorite_animals[1])
+favorite_animals.remove("monkey")
+
+favorite_animals.append("layon")
+for animal in favorite_animals:
+    print(f"i love {animal}")
+
+numbers = [1,2,3,4,5]
+number_sum = 0 
+for number in numbers :
+      number_sum += number
+print(f'The total is {number_sum}')
